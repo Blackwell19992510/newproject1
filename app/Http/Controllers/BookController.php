9a763f4ce3a7564cc4494a3da984d380cac8eebd
Blackwell19992510 <mucciidayday@gmail.com>
2023-11-17 10:43:26 +0300

    added Controller

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    //Create new Book
+    function createBook(Request $request){
+        // validate inputs
+        $request->validate([
+             'name'=>'required',
+             'pages'=>'required',
+             'IBN'=>'required',
+             'author'=>'required',
+             'category'=>'required',
+             'publisher'=>'required',
+             'yearofpublication'=>'required',
+         ]); 
+
+    /**
+     * Create Book using the Task Model Class
+     */
+    $book = Book::create([
+         'name'=>$request->name,
+         'pages'=>$request->pages,
+         'IBN'=>$request->IBN,
+         'author'=>$request->author,
+         'category'=>$request->category,
+         'publisher'=>$request->publisher,
+         'yearofpublicaton'=>$request->yearofpublication,
+         
+    ]);
+
+    $book = Book::find($book->id);
+        if(!$book){
+             return response(
+                 [
+                     "message"=>"failed",
+       ]);
+       }else{
+             return response(
+                 [
+                     "message"=>"success",
+                     "book"=>$book
+         ]);
+         }         
+    } 
+}
